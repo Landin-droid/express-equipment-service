8793c167064e561b49a74a3c177f85e058427986
Dmitry Lyamkin <lyamkin.dimentor1@mail.ru>
2026-09-20 23:02:42 +0700

    feat: add zod schemas to data models

--- src/routes/equipmentRoutes.js
+++ src/routes/equipmentRoutes.js
@@ -1,16 +1,52 @@
 import { Router } from "express";
 import equipmentController from "../controllers/equipmentController.js";
+import { validate } from "../middlewares/validate.js";
+import {
+  createEquipmentSchema,
+  updateEquipmentSchema,
+  listEquipmentQuerySchema,
+  idParamSchema,
+} from "../validators/equipmentValidators.js";
 
 const router = Router();
 
-router.get("/", equipmentController.list);
-router.post("/", equipmentController.create);
-router.get("/:id", equipmentController.getById);
-router.patch("/:id", equipmentController.update);
-router.delete("/:id", equipmentController.remove);
+router.get(
+  "/",
+  validate(listEquipmentQuerySchema, "query"),
+  equipmentController.list,
+);
+router.post(
+  "/",
+  validate(createEquipmentSchema, "body"),
+  equipmentController.create,
+);
+router.get(
+  "/:id",
+  validate(idParamSchema, "params"),
+  equipmentController.getById,
+);
+router.patch(
+  "/:id",
+  validate(idParamSchema, "params"),
+  validate(updateEquipmentSchema, "body"),
+  equipmentController.update,
+);
+router.delete(
+  "/:id",
+  validate(idParamSchema, "params"),
+  equipmentController.remove,
+);
 
-router.get("/:id/requests", equipmentController.getRequests);
+router.get(
+  "/:id/requests",
+  validate(idParamSchema, "params"),
+  equipmentController.getRequests,
+);
 
-router.get("/:id/weather", equipmentController.getWeather);
+router.get(
+  "/:id/weather",
+  validate(idParamSchema, "params"),
+  equipmentController.getWeather,
+);
 
 export default router;
--- src/routes/requestRoutes.js
+++ src/routes/requestRoutes.js
@@ -1,13 +1,47 @@
 import { Router } from "express";
 import requestController from "../controllers/requestController.js";
+import { validate } from "../middlewares/validate.js";
+import {
+  createRequestSchema,
+  updateRequestSchema,
+  changeStatusSchema,
+  listRequestsQuerySchema,
+} from "../validators/requestValidators.js";
+import { idParamSchema } from "../validators/equipmentValidators.js";
 
 const router = Router();
 
-router.get("/", requestController.list);
-router.post("/", requestController.create);
-router.get("/:id", requestController.getById);
-router.patch("/:id", requestController.update);
-router.patch("/:id/status", requestController.changeStatus);
-router.delete("/:id", requestController.remove);
+router.get(
+  "/",
+  validate(listRequestsQuerySchema, "query"),
+  requestController.list,
+);
+router.post(
+  "/",
+  validate(createRequestSchema, "body"),
+  requestController.create,
+);
+router.get(
+  "/:id",
+  validate(idParamSchema, "params"),
+  requestController.getById,
+);
+router.patch(
+  "/:id",
+  validate(idParamSchema, "params"),
+  validate(updateRequestSchema, "body"),
+  requestController.update,
+);
+router.patch(
+  "/:id/status",
+  validate(idParamSchema, "params"),
+  validate(changeStatusSchema, "body"),
+  requestController.changeStatus,
+);
+router.delete(
+  "/:id",
+  validate(idParamSchema, "params"),
+  requestController.remove,
+);
 
 export default router;
